ef4c0aa932132ec91aa88cce8ff9f8ff8043f0c0
Ali PIÇAKCI <alipicakci@gmail.com>
2020-06-16 21:15:47 +0300

    kod açıklamarı eklendi

--- Authentication.java
+++ Authentication.java
@@ -10,13 +10,46 @@ package com.pck.httppck;
 
 public interface Authentication {
 
+    /**
+     * @return token
+     */
     String getToken();
+
+    /*
+     * @return
+     */
     String getExpiresIn();
+    /**
+     * tokeni headere
+     * ekler
+     */
     void addHeaders();
+    /**
+     * mevcut http isteğini
+     * Authentication nesnesine
+     * set eder
+     */
     void setRequest(HttpRequest request);
+    /**
+     * yeni token alma sunucudan
+     */
     void newToken() throws Exception;
+    /**
+     * tokeni yenile
+     * TokenBasedAuthentication ile doğrulamada
+     * token yenilemeside #newToken() methodu ile
+     * gerçekleştirilmiştir
+     */
     void refreshToken();
+    /**
+     * tokeni bellekten ve
+     * cahceden temizler
+     */
     void clearToken();
+    /**
+     * doğrulama bilgilerini
+     * nesneye parametre olarak geçer
+     */
     void  setCredentials(Credentials credentials);
 
 }
